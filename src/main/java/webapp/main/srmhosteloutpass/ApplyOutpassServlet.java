7e8fccb172f24795770023a2cf941d4217cbabb0
kkroffl <rajakarthick366@gmail.com>
2025-12-25 18:23:04 +0530

    Major Update

--- src/main/java/webapp/main/srmhosteloutpass/ApplyOutpassServlet.java
+++ src/main/java/webapp/main/srmhosteloutpass/ApplyOutpassServlet.java
@@ -1,26 +1,43 @@
 package webapp.main.srmhosteloutpass;
 
-import java.io.*;
-import java.sql.*;
-import jakarta.servlet.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @WebServlet("/applyOutpass")
+@MultipartConfig
 public class ApplyOutpassServlet extends HttpServlet {
+    static Time returnTimeOf(String t) {
+        LocalTime time = LocalTime.parse(t);
+        return Time.valueOf(time);
+    }
+
+    static Date returnDateOf(String d) {
+        LocalDate date = LocalDate.parse(d);
+        return Date.valueOf(date);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
         res.setContentType("text/plain");
-
         String registeredNumber = req.getParameter("registeredNumber");
         String name = req.getParameter("name");
         String reason = req.getParameter("reason");
         String fromDate = req.getParameter("fromDate");
         String toDate = req.getParameter("toDate");
-
+        String leavingTime = req.getParameter("leavingTime");
+        String returnTime = req.getParameter("expectedReturnTime");
+        boolean isMedical = Boolean.parseBoolean(req.getParameter("isMedical").strip());
         if (registeredNumber == null || registeredNumber.isEmpty()) {
             res.getWriter().print("missing_rId");
             return;
@@ -37,34 +54,43 @@ public class ApplyOutpassServlet extends HttpServlet {
             ResultSet rs = findId.executeQuery();
             if (!rs.next()) {
                 res.getWriter().print("student_not_found");
+                System.out.println("student_not_found");
                 return;
             }
-
             int studentId = rs.getInt("id");
-
-            // 2. Insert outpass entry
-            PreparedStatement ps = conn.prepareStatement(
-                    "INSERT INTO outpass_requests (studentId, reason, from_date, to_date, name) " +
-                            "VALUES (?, ?, ?, ?, ?)"
-            );
-
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO outpass_requests (studentId, reason, leaving_date,leaving_time,applied_date,applied_time, expected_return_date, expected_return_time, name,type_of_outpass) " +
+                            "VALUES (?, ?, ?, ?, ?, ?,?,?,?,?)"
+                    ,Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, studentId);
             ps.setString(2, reason);
-            ps.setString(3, fromDate);
-            ps.setString(4, toDate);
-            ps.setString(5, name);
-
-            int rows = ps.executeUpdate();
-
-            if (rows > 0) {
-                res.getWriter().print("success");
-            } else {
-                res.getWriter().print("fail");
+            ps.setDate(3, returnDateOf(fromDate));
+            ps.setTime(4, returnTimeOf(leavingTime));
+            ps.setDate(5, Date.valueOf(LocalDate.now()));
+            ps.setTime(6, Time.valueOf(LocalTime.now()));
+            ps.setDate(7, returnDateOf(toDate));
+            ps.setTime(8, returnTimeOf(returnTime));
+            ps.setString(9, name);
+            String typeOfOutpass = isMedical ? "Medical" : "Other";
+            ps.setString(10,typeOfOutpass);
+            ps.executeUpdate();
+            if (isMedical) {
+                try (ResultSet rst = ps.getGeneratedKeys()) {
+                    if (rst.next()) {
+                        req.setAttribute("requestID", rst.getInt(1));
+                        RequestDispatcher rd = req.getRequestDispatcher("/medical_outpass");
+                        rd.forward(req, res);
+                    } else {
+                        System.out.println("Failed to retrieve requestID");
+                        throw new SQLException("Failed to retrieve requestID");
+                    }
+                } catch (SQLException e) {
+                    res.getWriter().write("error|" + e.getMessage());
+                }
             }
-
         } catch (Exception e) {
-            e.printStackTrace();
             res.getWriter().print("error");
+        }finally {
+            res.getWriter().print("success");
         }
     }
 }
--- src/main/java/webapp/main/srmhosteloutpass/DisplayOutpassServlet.java
+++ src/main/java/webapp/main/srmhosteloutpass/DisplayOutpassServlet.java
@@ -1,54 +1,80 @@
 package webapp.main.srmhosteloutpass;
 
-import jakarta.servlet.*;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Arrays;
+
 @WebServlet("/student_outpasses")
+@MultipartConfig
 public class DisplayOutpassServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException {
-        doGet(req,res);
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        doGet(req, res);
     }
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
-
         res.setContentType("application/json");
-
+        req.setCharacterEncoding("UTF-8");
+        req.getParameterMap().forEach((k, v) ->
+                System.out.println(k + " = " + Arrays.toString(v))
+        );
         String regNo = req.getParameter("registeredNumber");
-        if (regNo == null || regNo.isEmpty()) {
+        if (regNo == null || regNo.trim().isEmpty()) {
+            System.out.println("Registered Number illaya?");
             res.getWriter().print("[]");
             return;
         }
-
         try (Connection conn = DBConnector.getConnection()) {
-
-            PreparedStatement ps = conn.prepareStatement("SELECT reason, from_date, to_date, status FROM outpass_requests WHERE studentId = (SELECT id FROM students WHERE registeredNumber = ?)");
-
+            PreparedStatement ps = conn.prepareStatement("SELECT requestId,reason,applied_date,applied_time, leaving_date,leaving_time, expected_return_date,expected_return_time,type_of_outpass,proof_img, status FROM outpass_requests WHERE studentId = (SELECT id FROM students WHERE registeredNumber = ?)");
             ps.setString(1, regNo);
             ResultSet rs = ps.executeQuery();
-
+            if(!rs.next()){
+                System.out.println("No roww aaah");
+                res.getWriter().print("[]");
+                return;
+            }
             StringBuilder json = new StringBuilder("[");
             boolean first = true;
-
-            while (rs.next()) {
+            do{
                 if (!first) json.append(",");
                 first = false;
-
-                json.append("{")
-                        .append("\"reason\":\"").append(rs.getString("reason")).append("\",")
-                        .append("\"from_date\":\"").append(rs.getString("from_date")).append("\",")
-                        .append("\"to_date\":\"").append(rs.getString("to_date")).append("\",")
-                        .append("\"status\":\"").append(rs.getString("status")).append("\"")
-                        .append("}");
+                json.append("""
+            {
+              "id": "%s",
+              "reason": "%s",
+              "applied_date": "%s",
+              "applied_time": "%s",
+              "from_date": "%s",
+              "from_time": "%s",
+              "to_date": "%s",
+              "to_time": "%s",
+              "type_of_outpass": "%s",
+              "status": "%s"
             }
-
+            """.formatted(
+                        rs.getString("requestId"),
+                        rs.getString("reason"),
+                        rs.getString("applied_date"),
+                        rs.getString("applied_time"),
+                        rs.getString("leaving_date"),
+                        rs.getString("leaving_time"),
+                        rs.getString("expected_return_date"),
+                        rs.getString("expected_return_time"),
+                        rs.getString("type_of_outpass"),
+                        rs.getString("status")
+                ));
+            }while (rs.next());
             json.append("]");
             res.getWriter().write(json.toString());
-
         } catch (Exception e) {
+            System.out.println("SQL error aaaah");
             res.getWriter().print("[]");
         }
     }
